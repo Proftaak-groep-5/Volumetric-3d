fc170b6a56c4b12950523182a6119d0c04ef40d2
SquidgyPants <115804504+SquidgyPants@users.noreply.github.com>
2026-06-10 15:59:04 +0200

    coding complete, test functionality

--- platform/backend/app/services/museum.py
+++ platform/backend/app/services/museum.py
@@ -1,6 +1,7 @@
 import uuid
 
 from sqlalchemy.ext.asyncio import AsyncSession
+from ...events.event_types import MuseumCreatedEvent
 
 from ..crud import museum as museum_crud
 from ..crud import recording as recording_crud
@@ -16,10 +17,24 @@ async def get_all_museums(db):
 
 async def create_museum(
         db: AsyncSession,
+        publisher: EventPublisher,
         name: str,
         description: str
 ):
-    return await museum_crud.create_museum(db, name=name, description=description)
+    museum = await museum_crud.create_museum(db, name=name, description=description)
+
+    event = MuseumCreatedEvent(
+        name=name,
+        description=description
+    )
+
+    await publisher.publish(
+        exchange_name="museum.events",
+        routing_key="museum.created",
+        event=event.model_dump()
+    )
+
+    return museum
 
 async def get_museum_by_id(db, museumid):
     museum = await museum_crud.get_museum_by_id(db, museumid)
--- platform/backend/events/consumer.py
+++ platform/backend/events/consumer.py
@@ -4,3 +4,9 @@ def callback(ch, method, properties, body):
     event = json.loads(body)
 
     print ("Received:", event)
+
+async def handle_museum_created(message: aio_pika.IncomingMessage):
+    async with message.process():
+        event = json.loads(message.body)
+
+        print("Museum created event received:", event)
